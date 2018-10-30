4f846e649ad2f50b44b9dcc71f10df5cf0aba344
mocivnik10 <iztok.mocivnik@poviolabs.com>
2018-10-30 11:47:22 +0100

    Add get users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const _ = require('lodash')
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+const {authenticate} = require('./middleware/authenticate')
 
 var app = express();
 
@@ -47,6 +48,32 @@ app.get('/todos/:id', (req, res) => {
   })
 })
 
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id
+  var body = _.pick(req.body, ['text', 'completed'])
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send()
+    }
+
+    res.send({todo})
+  }).catch((e) => {
+    res.status(400).send()
+  })
+})
+
 app.post('/users', (req, res) => {
   // var user = new User({
   //   email: req.body.email,
@@ -62,6 +89,10 @@ app.post('/users', (req, res) => {
   }).catch((e) => {
     res.status(400).send(e)
   })
+})
+
+app.get('/users/me', authenticate, (req,res) => {
+  res.send(req.user);
 })
 
 app.listen(3000, () => {
