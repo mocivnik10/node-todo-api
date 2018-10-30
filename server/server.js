9fa6cd53df09c67f1d8a325d9025aeb63c1345f8
mocivnik10 <iztok.mocivnik@poviolabs.com>
2018-10-30 10:34:59 +0100

    Add pust /users route and token method

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb')
+const _ = require('lodash')
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -43,6 +44,23 @@ app.get('/todos/:id', (req, res) => {
     res.send({todo})
   }, (e) => {
     res.status(400).send()
+  })
+})
+
+app.post('/users', (req, res) => {
+  // var user = new User({
+  //   email: req.body.email,
+  //   password: req.body.password
+  // })
+  var body = _.pick(req.body, ['email', 'password'])
+  var user = new User(body);
+  
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user)
+  }).catch((e) => {
+    res.status(400).send(e)
   })
 })
 
